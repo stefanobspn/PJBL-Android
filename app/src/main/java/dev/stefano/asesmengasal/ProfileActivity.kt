5cd9faa80fa2961d0da4b104dcb674adc1b1f992
Stefano Nirwana <stefano.bspn@gmail.com>
2025-11-24 08:35:17 +0700

    refactor: Move edit profile functionality to FormActivity
    This commit refactors the "Edit Profile" functionality by redirecting the user to the `FormActivity` instead of handling it within the `ProfileActivity`.
    
    Changes include:
    - Adding an `OnClickListener` to the `edit_profile_button` in `ProfileActivity.kt`.
    - Implementing the listener to create an `Intent` that starts `FormActivity` when the button is clicked.

--- app/src/main/java/dev/stefano/asesmengasal/ProfileActivity.kt
+++ app/src/main/java/dev/stefano/asesmengasal/ProfileActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -32,6 +33,12 @@ class ProfileActivity : BaseActivity() {
             val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
             pickImage.launch(intent)
+        }
+
+        val editProfileButton = findViewById<Button>(R.id.edit_profile_button)
+        editProfileButton.setOnClickListener {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
         }
     }
 }
